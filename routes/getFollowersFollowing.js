14baffd6b691192a921a444716494887d814e20d
smit659 <smittsoni659@gmail.com>
2022-09-08 20:06:55 +0530

    followers following completed

--- routes/getFollowersFollowing.js
+++ routes/getFollowersFollowing.js
@@ -7,7 +7,7 @@ const jwt = require('jsonwebtoken');
 router.get('/:author',(req,res)=>{
     console.log(req.params.author);
     OpiniateUsersModel.findOne({name:req.params.author},(err,result)=>{
-        console.log(result.incoming);
+       
         const ans={followersNumber:result.followers.length,followingNumber:result.following.length,followers:result.followers,following:result.following}
         res.send(ans);
     });
